2fd40b474b34bdb0338c7d017213c6511027aa35
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-05-26 16:06:39 +0100

    dev: mongoose and schema for db

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 
@@ -6,10 +7,26 @@ const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = process.env.MONGO_URL;
+
 const server = http.createServer(app);
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
 
 // load data on startup of server
 async function startServer() {
+  mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
   await loadPlanetsData();
 
   server.listen(PORT, () => {
