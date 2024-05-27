f8ab04491e293711b0b78491f55e2f32e0db0738
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-05-27 11:33:53 +0100

    update: separation of concerns for mongodb services into services

--- server/src/server.js
+++ server/src/server.js
@@ -1,30 +1,18 @@
 const http = require("http");
-const mongoose = require("mongoose");
-require("dotenv").config();
 
 const app = require("./app");
+
+const { mongoConnect } = require("./services/mongo");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8001;
-const MONGO_URL = process.env.MONGO_URL;
 
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready!");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 // Load data on startup of server
 async function startServer() {
-  mongoose.connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
