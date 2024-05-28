aa72c6d51b2b9c0894493cecb2d6f26a39e1afcd
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-05-28 19:22:54 +0100

    dev: sort API in ascending order

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -5,19 +5,6 @@ const planets = require("./planets.mongo");
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launch = {
-  flightNumber: 100,
-  mission: "Kepler Exploration X",
-  rocket: "Explorer IS1",
-  launchDate: new Date("December 27, 2030"),
-  target: "Kepler-1652 b",
-  customers: ["ZTM", "NASA"],
-  upcoming: true,
-  success: true,
-};
-
-saveLaunch(launch);
-
 const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
 
 async function populateLaunches() {
@@ -119,10 +106,11 @@ async function getLatestFlightNumber() {
   return latestLaunch.flightNumber;
 }
 
-// limits and skip to paginate API
+// limits and skip to paginate API, and sort with ascending  values
 async function getAllLaunches(skip, limit) {
   return await launchesDatabase
     .find({}, { _id: 0, __v: 0 })
+    .sort({ flightNumber: 1 })
     .skip(skip)
     .limit(limit);
 }
